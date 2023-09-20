b4a03a1ecd51d9e53cfc206dc18431098eaeac36
zfei <zhenghao.fei@murobotix.com>
2023-09-19 22:35:33 -0700

    pkg created

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='rosbagpy',
+    version='0.0.1',
+    description='a pip-installable rosbag python utlity package',
+    license='Apache-2.0 license',
+    packages=['rosbagpy'],
+    author='Zhenghao Fei',
+    author_email='zfei@zju.edu.cn',
+    keywords=['rosbagpy'],
+    url='https://github.com/bgse/rosbagpy',
+    install_requires=['scipy'],
+)
